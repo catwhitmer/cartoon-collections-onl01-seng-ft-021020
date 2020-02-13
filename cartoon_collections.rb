cd01aacdc0b28e9c2180aa2368d4969462ede71b
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 21:00:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
