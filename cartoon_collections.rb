27e3c4d019157df3c8c61b3c1cddda19bd3fff35
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 20:58:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,18 +7,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
- i = 0
- long_words = []
- while i < calls.length
-   long_words << yield(calls[i])
- i += 1
- end
+
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-
-# calls.select {|word| word.length > 4}
