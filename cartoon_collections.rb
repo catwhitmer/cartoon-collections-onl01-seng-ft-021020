625a4434612b225f09b5b5c8e69ee19fdc4ebdbb
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 21:01:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,4 +15,6 @@ end
 def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |type|
+    cheese_types.include?(type)
 end
